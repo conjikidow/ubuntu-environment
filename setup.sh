76797f5c8890afc1c81ddd62163ad0da90e7a4d7
Riki Nakamura <riki.nakamura.edu@gmail.com>
2023-06-07 13:45:54 +0900

    Fix bug (add latexindent installation)

--- setup.sh
+++ setup.sh
@@ -87,6 +87,13 @@ if [[ $REPLY == $'\n' ||  $REPLY == [yY] ]]; then
     export LATEXDIR=${ENVDIR}/latex
     export TEXMFHOME=${LATEXDIR}/texmf
     ln -s "$(pwd)/dotfiles/latexmkrc" ${HOME}/.latexmkrc
+    sudo apt-get install -y perl
+    sudo cpan -i App::cpanminus
+    sudo cpanm YAML::Tiny
+    sudo cpanm File::HomeDir
+    sudo cpanm Unicode::GCString
+    sudo cpanm Log::Log4perl
+    sudo cpanm Log::Dispatch::File
     echo "paths:
     - ${LATEXDIR}/latexindent/userSettings.yaml" > ${HOME}/.indentconfig.yaml
 else
